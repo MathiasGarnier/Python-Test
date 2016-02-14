b5e8d1d45e4846c1d17a92ded07dedf44c805549
Mathias Garnier - Yapix <mathias.garnier69000@gmail.com>
2016-02-14 16:32:46 +0100

    Create test_0007.py

---
+++ test_0007.py
@@ -0,0 +1,41 @@
+def damage(lostHeart, heart):
+
+    """
+    For the damage !
+    :param lostHeart: When player lose heart !
+    :param heart: Player heart !
+    :return:
+    """
+    
+    while lostHeart < player.heart:
+        heart -= 1
+
+
+def player(heart):
+
+    """
+    For the player in him
+    :param heart: Player heart !
+    :return:
+    """
+    
+    player.earth = 20
+
+    if player.heart < 0:
+        return False
+    elif player.heart > 20:
+        return False
+
+def life(heart, maxLife, minLife):
+
+    """
+    For the player life !
+    :param heart: player heart in real time x)
+    :param maxLife: 20 is the max Life for a basic player
+    :param minLife: 0 is the min Life for a basic player
+    :return:
+    """
+
+    maxLife = 20
+    minLife = 0
+    heart = player.heart
