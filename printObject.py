c900113f62c8b45a10c0951bbe210cff99f24b38
Mathias Garnier - Yapix <mathias.garnier69000@gmail.com>
2016-08-24 13:26:28 +0200

    Create printObject.py

---
+++ printObject.py
@@ -0,0 +1,15 @@
+# -*-coding:Latin-1 -*
+
+#def getObjectType(object):
+
+#    return type(object)
+
+def printObject(object):
+
+    for i in object:
+        print(i)
+
+#dataObject = input()
+#x = getObjectType(dataObject)
+
+printObject("Ola quetal")
